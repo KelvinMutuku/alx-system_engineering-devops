a0bc8b6ebe9dad709b6f3cd7b400dbe46bd0c936
KELVIN_MUTUKU <118681525+KelvinMutuku@users.noreply.github.com>
2023-06-23 16:48:15 +0300

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-exec { 'install_flask':
-  command  => '/usr/bin/pip3 install flask==2.1.0',
-  creates  => '/usr/local/lib/python3.x/dist-packages/flask',
-  provider => 'shell',
-}
+# install puppet-lint -v 2.1.0
 
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.1.0,
+}
