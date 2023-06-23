ed70ce6e7ec5255ba2c0f8bb560e373f6dafa85b
KELVIN_MUTUKU <118681525+KelvinMutuku@users.noreply.github.com>
2023-06-23 16:59:22 +0300

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,21 +1,5 @@
-package { 'flask':
-  ensure => '2.1.0',
-}
+# install puppet-lint -v 2.1.0
 
-exec { 'install_puppet_lint':
-  command => '/usr/bin/apt-get -y install puppet-lint',
-}
-
-exec { 'puppet_apply':
-  command => 'puppet apply 1-install_a_package.pp',
-  path    => '/bin:/sbin:/usr/bin:/usr/sbin',
-  require => [Package['flask'], Exec['install_puppet_lint']],
-  logoutput => true,
-}
-
-exec { 'check_flask_version':
-  command => 'flask --version',
-  path    => '/bin:/sbin:/usr/bin:/usr/sbin',
-  require => Exec['puppet_apply'],
-  logoutput => true,
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.1.0',
 }
